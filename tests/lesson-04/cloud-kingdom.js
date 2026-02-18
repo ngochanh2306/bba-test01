a95d60a0f2c91cae0a24fa5dbd23dac770e3162c
test 01 - Hanh <nguyenhoangngochanh679@gmail.com>
2026-02-19 01:29:32 +0700

    feat: add solution for challenge 03

---
+++ tests/lesson-04/cloud-kingdom.js
@@ -0,0 +1,5 @@
+const powerUp = "mushroom";
+let effect = powerUp === "mushroom" ? "Mario becomes Super!":
+            powerUp === "flower" ? "Mario can shoot fireballs!":
+            powerUp === "star" ? "Mario is invincible!": "Mario is normal!"
+console.log(effect);
