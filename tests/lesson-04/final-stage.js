054984fb4999a1c5f4d3da53bac50efdb0c69864
test 01 - Hanh <nguyenhoangngochanh679@gmail.com>
2026-02-20 23:13:40 +0700

    feat: add Stage 05

---
+++ tests/lesson-04/final-stage.js
@@ -0,0 +1,16 @@
+function findPairsDivisibleBy17(){
+    let count = 0;
+
+    for(let i = 1; i<101; i++){
+        for(let j = i+1; j<101; j++){
+            if((i+j)%17 === 0){
+                console.log(`(${i} ${j}) = ${i+j}`);
+                count++;
+            }
+        }
+    }
+
+    console.log(`Tổng cộng ${count} cặp`);
+}
+
+findPairsDivisibleBy17();
